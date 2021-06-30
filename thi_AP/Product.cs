37bc15f5e699af0f51b6f5c2fb9d4f142e0ac2fc
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-06-30 09:27:36 +0700

    make it better

---
+++ thi_AP/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace thi_AP
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public Product(){}
+
+        public Product(string productId, string productName, double price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{ProductId} \t {ProductName} \t ${Price}";
+        }
+    }
+}
